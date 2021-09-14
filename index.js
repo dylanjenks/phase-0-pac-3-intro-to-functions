6ce4b7a45ddece0f0a6c511bc1ce01599189b50f
dylanjenks <dylan.jenks92@gmail.com>
2021-09-14 00:01:51 -0400

    Saving Progress

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+  } sayHello(); 
+
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  } 
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  } 
+  
+  sayHelloToIsabel()
+  sayHelloToSofia()
+  sayHelloToBrendan()
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  } 
+  say("Julio", "hello"); 
